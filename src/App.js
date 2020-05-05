a8794231c396221de5f927708ed34af452677a75
Alejandro Busse <al3busse@gmail.com>
2020-05-05 00:57:22 -0300

    breaks:true

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import React from "react";
 import marked from "marked";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-i;
 
-export default class App extends Component {
+marked.setOptions({
+  breaks: true,
+});
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
   render() {
     return <div></div>;
   }
