6ef9232b6905ddae6f9686d1d3899b4b03934b63
Alejandro Busse <al3busse@gmail.com>
2020-05-06 03:30:25 -0300

    Updates

---
+++ precache-manifest.833b35ca49350e6acf1043c9fd72633f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f8a47f7e504c6fa9fa69caf8970700c",
+    "url": "/Markdown-Previewer/index.html"
+  },
+  {
+    "revision": "d24d99854eb31030f3eb",
+    "url": "/Markdown-Previewer/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "99b7e280f58b25afaabc",
+    "url": "/Markdown-Previewer/static/css/main.e24eead7.chunk.css"
+  },
+  {
+    "revision": "d24d99854eb31030f3eb",
+    "url": "/Markdown-Previewer/static/js/2.87aa0b70.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Markdown-Previewer/static/js/2.87aa0b70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99b7e280f58b25afaabc",
+    "url": "/Markdown-Previewer/static/js/main.07d0516a.chunk.js"
+  },
+  {
+    "revision": "ef6511e8b465c91a80bb",
+    "url": "/Markdown-Previewer/static/js/runtime-main.327d67d0.js"
+  }
+]);
